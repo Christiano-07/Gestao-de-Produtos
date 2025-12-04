d425f53a629ecc2b006dc770079154908b2e5809
Christiano-07 <cristiano.mello@ba.estudante.senai.br>
2025-12-04 04:12:57 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
-// firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyACQoQHnnzKupSK4lxqTJZBy3bLIzC5ybY",
@@ -12,6 +13,18 @@ const firebaseConfig = {
   appId: "1:966717709137:web:4925b688facd98d7f09170"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Inicialização do Firebase
+
+let app;
+try {
+  app = initializeApp(firebaseConfig);
+} catch (e) {
+  console.error("Erro ao inicializar Firebase:", e);
+}
+
+// Serviços principais
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Exportações para uso em todo o sistema
+export { auth, db };
